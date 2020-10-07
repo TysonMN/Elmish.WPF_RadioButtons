0fd431e6e515a1120b3565a568d9adb6afe0469d
Tyson Williams <tyson.williams@blocherconsulting.com>
2020-10-07 11:23:35 -0500

    added FieldTypeOptional

---
+++ Source/FieldTypeOptional.Views/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static ViewModel.Program;
+
+namespace FieldTypeOptional.Views {
+  public static class Program {
+    [STAThread]
+    public static void Main() =>
+      main(new MainWindow());
+  }
+}
---
+++ Source/FieldTypeOptional/Program.fs
@@ -0,0 +1,44 @@
+﻿module ViewModel.Program
+
+open Elmish.WPF
+
+
+type DU1 = A | B
+type DU2 = C | D
+
+type Model =
+  { DU1: DU1 option
+    DU2: DU2 option }
+
+type Msg =
+  | SetDU1 of DU1 option
+  | SetDU2 of DU2 option
+
+let init =
+  { DU1 = Some A
+    DU2 = Some C }
+
+let update msg m =
+  match msg with
+  | SetDU1 du1 -> { m with DU1 = du1 }
+  | SetDU2 du2 -> { m with DU2 = du2 }
+
+let filter ma = function
+  | true -> ma
+  | false -> None
+
+let bindingDU1 x = Binding.twoWay((fun m -> m.DU1 = Some x), x |> Some |> filter >> SetDU1)
+let bindingDU2 x = Binding.twoWay((fun m -> m.DU2 = Some x), x |> Some |> filter >> SetDU2)
+
+let bindings () = [
+  "IsA" |> bindingDU1 A
+  "IsB" |> bindingDU1 B
+  "IsC" |> bindingDU2 C
+  "IsD" |> bindingDU2 D
+]
+
+let designVm = ViewModel.designInstance init (bindings ())
+
+let main window =
+  Program.mkSimpleWpf (fun () -> init) update bindings
+  |> Program.runWindow window
