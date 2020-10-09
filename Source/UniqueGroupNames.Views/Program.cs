9b8711b3b1b2656ad8ba79e3ee1f9e18f817aa45
Tyson Williams <tyson.williams@blocherconsulting.com>
2020-10-09 06:24:14 -0500

    added UniqueGroupNames

---
+++ Source/UniqueGroupNames.Views/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static ViewModel.Program;
+
+namespace UniqueGroupNames {
+  public static class Program {
+    [STAThread]
+    public static void Main() =>
+      main(new MainWindow());
+  }
+}
---
+++ Source/UniqueGroupNames/Program.fs
@@ -0,0 +1,40 @@
+﻿module ViewModel.Program
+
+open Elmish.WPF
+
+
+type DU1 = A | B
+type DU2 = C | D
+
+type Model =
+  { DU1: DU1
+    DU2: DU2 }
+
+type Msg =
+  | SetDU1 of DU1
+  | SetDU2 of DU2
+
+let init =
+  { DU1 = A
+    DU2 = C }
+
+let update msg m =
+  match msg with
+  | SetDU1 du1 -> { m with DU1 = du1 }
+  | SetDU2 du2 -> { m with DU2 = du2 }
+
+let bindingDU1 x = Binding.twoWay((fun m -> m.DU1 = x), (fun _ -> x |> SetDU1))
+let bindingDU2 x = Binding.twoWay((fun m -> m.DU2 = x), (fun _ -> x |> SetDU2))
+
+let bindings () = [
+  "IsA" |> bindingDU1 A
+  "IsB" |> bindingDU1 B
+  "IsC" |> bindingDU2 C
+  "IsD" |> bindingDU2 D
+]
+
+let designVm = ViewModel.designInstance init (bindings ())
+
+let main window =
+  Program.mkSimpleWpf (fun () -> init) update bindings
+  |> Program.runWindow window
